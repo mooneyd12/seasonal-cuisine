7c232200db392015cadb677d5f432694cbb53f71
Ricardo Mallols <ricardo.mallols@gmail.com>
2018-12-04 11:45:09 +0000

    Changing title (#2)
    * Add some exclamation marks to the title
    
    * Setting a more generic title, thinking about the other teams

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <h1>Welcome to the Alpha Team app</h1>
+            <h1>Welcome to the Team app!!!!</h1>
           </p>
           <a
             className="App-link"
