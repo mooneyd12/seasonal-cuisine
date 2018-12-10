014ca74b4ebd89794aea769f02dbd1b5b05ac56c
alexandra-pascal <33359008+alexandra-pascal@users.noreply.github.com>
2018-12-10 16:08:40 +0000

    Created the footer (#15)
    * amended title of the app in package.json
    
    * create component folder and chanhed the paths
    
    * create header and add logo and add css for mobile and desktop

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import Header from './../Header/Header'
-import './App.css';
+import React, { Component } from "react";
+import Header from "./../Header/Header";
+import Footer from "./../Footer/Footer";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header/>
+        <Header />
         <h1>test</h1>
+        <Footer />
       </div>
     );
   }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer(props) {
+  return (
+    <header className="footer">
+      <p>©Designed by Charlie Team | All rights reserved</p>
+    </header>
+  );
+}
