e322b3b6a8addba0261ada69a1431c84698a2d6b
alexandra-pascal <33359008+alexandra-pascal@users.noreply.github.com>
2018-12-11 20:55:34 +0000

    create placeholder for the dropdown (#25)
    * create placeholder for the dropdown
    
    * deleting duplicated divs in home page

--- src/Components/Home/Dropdown.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function Dropdown(props) {
-  return (
-    <div className="dropdown">
-    </div>
-  );
-}
--- src/Components/Home/HeroText.js
+++ src/Components/Home/HeroText.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./HeroText.css";
 
- export default function Routes() {
+export default function HeroText() {
   return (
     <div className="wrap-text">
       <p className="home-title">Choose your favourite dish for christmas</p>
       <p className="home-text">
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
         since the 1500s
       </p>
     </div>
