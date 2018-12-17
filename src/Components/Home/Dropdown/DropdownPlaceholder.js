89ea6e5dda79af5c898db5284e410af1d0ad735e
Marta Bekyarska <martatsoneva@yahoo.com>
2018-12-17 15:19:35 +0000

    bound the menu options with filter functions

--- src/Components/Home/Dropdown/DropdownPlaceholder.js
+++ src/Components/Home/Dropdown/DropdownPlaceholder.js
@@ -2,26 +2,13 @@ import React from "react";
 import "./DropdownPlaceholder.css";
 
 export default class DropdownPlaceholder extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      recipes: [],
-      filteredCountries: []
-    }
-  }
-
-  handleClick(event) {
-    this.setState({
-      country : event.target.value
-    })
-  }
 
   render() {
     return (
       <div className="dropdown-placeholder">
         <div className="filter">
-          <select name="countries" className="options" defaultValue="" onChange={this.handleClick = this.handleClick.bind(this)}>
+          <select name="countries" className="options" defaultValue="" 
+          onChange={event => this.props.onCountrySelected(event.target.value)}>
             <option value="">Select country</option>
             <option value="Bulgaria">Bulgaria</option>
             <option value="Romania">Romania</option>
@@ -29,7 +16,8 @@ export default class DropdownPlaceholder extends React.Component {
             <option value="Trinidad &amp; Tobago">Trinidad & Tobago</option>
             <option value="China">Pakistan</option>
           </select>
-          <select name="course" className="options" defaultValue="">
+          <select name="course" className="options" defaultValue="" 
+          onChange={event => this.props.onCourseSelected(event.target.value)}>
             <option value="">Select course</option>
             <option value="main">Main</option>
             <option value="dessert">Dessert</option>
