bb7386f56658963a1b8e44030b1923e9c1f7a225
alexandra-pascal <33359008+alexandra-pascal@users.noreply.github.com>
2018-12-17 20:49:21 +0000

    scroll to top when changing pages (#47)
    Neat fix. nice work alex 👍approving changes.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "react-router-scroll-top";
 import Header from "./../Header/Header";
 import Routes from "./../Routes";
 import Footer from "./../Footer/Footer";
@@ -9,13 +10,15 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <Header />
-          <main>
-            <Routes />
-          </main>
-          <Footer />
-        </div>
+        <ScrollToTop>
+          <div className="App">
+            <Header />
+            <main>
+              <Routes />
+            </main>
+            <Footer />
+          </div>
+        </ScrollToTop>
       </Router>
     );
   }
