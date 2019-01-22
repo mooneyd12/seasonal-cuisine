34998ad54177ae0715ef2c85b578c9999d83ccb9
Muna Mohamed <munamohamed94@hotmail.co.uk>
2019-01-22 23:49:00 +0000

    Updates

---
+++ precache-manifest.151c3b49db2a3e9fd4228c2c42eadcf3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cbd99d251efa4d2dd91d228437810a8",
+    "url": "/seasonal-cuisine/static/media/cookies2.4cbd99d2.jpg"
+  },
+  {
+    "revision": "7bdff4c7fd7fcb6a44cd",
+    "url": "/seasonal-cuisine/static/css/main.0270f0e6.chunk.css"
+  },
+  {
+    "revision": "9ce4b36a639abdd292ef",
+    "url": "/seasonal-cuisine/static/js/1.9ce4b36a.chunk.js"
+  },
+  {
+    "revision": "0784e9ce00cc1b9a6344",
+    "url": "/seasonal-cuisine/static/js/runtime~main.0784e9ce.js"
+  },
+  {
+    "revision": "9c3dc666a3d4971cbd342024c587ea39",
+    "url": "/seasonal-cuisine/static/media/chicken.9c3dc666.jpg"
+  },
+  {
+    "revision": "1fd31968719c79f60be7fc6c2b318c38",
+    "url": "/seasonal-cuisine/static/media/cookies.1fd31968.jpg"
+  },
+  {
+    "revision": "7bdff4c7fd7fcb6a44cd",
+    "url": "/seasonal-cuisine/static/js/main.7bdff4c7.chunk.js"
+  },
+  {
+    "revision": "bb5d028b0fed3945c5be90ee280852d5",
+    "url": "/seasonal-cuisine/static/media/meat.bb5d028b.jpg"
+  },
+  {
+    "revision": "db87f7696ec531d5ce39ec9aa416d070",
+    "url": "/seasonal-cuisine/static/media/meat2.db87f769.jpg"
+  },
+  {
+    "revision": "69e02093b55540027b72b93d8f27d29e",
+    "url": "/seasonal-cuisine/static/media/logoSC.69e02093.png"
+  },
+  {
+    "revision": "b8697c3321255f6dabe191c2c320a6a8",
+    "url": "/seasonal-cuisine/static/media/hero-img.b8697c33.jpg"
+  },
+  {
+    "revision": "54aa229efe8a5bc5d4fc891ad6c3faf8",
+    "url": "/seasonal-cuisine/index.html"
+  }
+];
--- src/Components/NotFound/NotFoundPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NotFound() {
-  return (
-    <div>
-      <p>Not found page</p>
-    </div>
-  );
-}
